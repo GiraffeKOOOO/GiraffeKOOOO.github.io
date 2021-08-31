60a4cd2e262e5b283b5ac1b659fe7465886f8057
Pascal Stepien <pstepien@hotmail.co.uk>
2021-09-01 00:56:40 +0100

    added a round image and the introduction text

---
+++ src/components/ProjectCarousel.js
@@ -0,0 +1,17 @@
+// import react
+import React from 'react';
+// import necessary components
+
+// import styling
+import '../css/ProjectCarousel.css';
+// import media
+
+function ProjectCarousel() {
+    return (
+        <div id="carousel-wrapper">
+            <p>project carousel</p>
+        </div>
+    );
+}
+
+export default ProjectCarousel;
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,7 +1,9 @@
 // import react
 import React from 'react';
+import { Col, Container, Row } from 'react-bootstrap';
 // import necessary components
-
+import ProjectCarousel from '../components/ProjectCarousel';
+import Intro from '../components/Intro';
 // import styling
 import '../css/HomePage.css';
 // import media
@@ -9,9 +11,22 @@ import '../css/HomePage.css';
 
 function HomePage() {
     return (
-      <div>
-          <h1>Home Page</h1>
-      </div>
+      <Container id="home-page-wrapper" fluid>
+        <Row id="row-intro">
+          <Col id="col-left" xs={3} md={3} lg={3} xl={3} xxl={3}/>
+          <Col id="col-middle" xs={6} md={6} lg={6} xl={6} xxl={6}>
+            <Intro/>
+          </Col>
+          <Col id="col-right" xs={3} md={3} lg={3} xl={3} xxl={3}/>
+        </Row>
+        <Row id="row-carousel">
+          <Col id="col-left" xs={1} md={1} lg={1} xl={1} xxl={1}/>
+          <Col id="col-middle" xs={10} md={10} lg={10} xl={10} xxl={10}>
+            <ProjectCarousel/>
+          </Col>
+          <Col id="col-right" xs={1} md={1} lg={1} xl={1} xxl={1}/>
+        </Row>
+      </Container>
     );
 }
 
