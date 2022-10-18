7802b441728836c75d990e50bdf5e6f46d831276
Pascal Stepien <pstepien@hotmail.co.uk>
2022-10-18 13:54:05 +0100

    added necessary libraries and structure
    - added react-router-dom, I think that's what got the 6 package issues
    - added react-bootstrap
    - added tailwind
    - added a router structure to the app.js file
    - added the react-bootstrap css link to the index file
    - added a navbar to the home page, ready to get started

---
+++ src/components/NavigationBar.js
@@ -0,0 +1,31 @@
+import '../App.css';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import logo512 from '../media/logo512.png';
+
+function NavigationBar() {
+  return (
+    <>
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="/">
+            <img
+              alt="portfolio-page-logo"
+              src={logo512}
+              width="30"
+              height="30"
+              className="d-inline-block align-top"
+            />{' '}
+            Paskal Stepien
+          </Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="/">Home</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+    </>
+  );
+}
+
+export default NavigationBar;
---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+import '../App.css';
+import NavigationBar from '../components/NavigationBar.js';
+
+function Home() {
+  return (
+    <>
+      <NavigationBar/>
+      <p>Hello</p>
+    </>
+  )
+}
+
+export default Home;
