40e528f499e06e7162e7361bfa405842735b5a5a
Pascal Stepien <pstepien@hotmail.co.uk>
2022-11-05 11:34:27 +0000

    finished working on the github banner

--- src/components/GithubBanner.js
+++ src/components/GithubBanner.js
@@ -5,9 +5,14 @@ import GithubBannerImage from '../media/github-banner-white.png';
 function GithubBanner() {
   return (
     <> 
-        <a>
+        <a
+            href='https://github.com/GiraffeKOOOO'
+            target='_blank'
+            rel='noreferrer noopener'
+            className='no-underline text-white'
+        >
             <Card>
-                <Card.Body className='p-2 h-[100px] bg-slate-700'>
+                <Card.Body className='p-2 h-[100px] bg-gray-800 hover:bg-gray-700'>
                     <div className='flex flex-row'>
                         <img className='w-[85px] h-[85px] rounded-full border-2 border-gray-350' src='https://avatars.githubusercontent.com/u/33908517?v=4' alt='github-profile'/>
                         <div className='flex-1 ml-5 my-2'>
