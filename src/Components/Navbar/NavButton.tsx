a5ed07d7531f32b56d976e0e054a2a3edd5d65fd
Paskal Stepien <33908517+GiraffeKOOOO@users.noreply.github.com>
2025-05-20 20:21:34 +0100

    feat(cv): adding cv link to nav button (#15)
    - added an optional prop to the navbutton for cv
    - added a link to the cv

--- src/Components/Navbar/NavButton.tsx
+++ src/Components/Navbar/NavButton.tsx
@@ -6,9 +6,10 @@ import { Colours } from 'Components/Shared/Colours';
 type NavButtonProps = {
   btnName: NavButtonName;
   sectionRef?: RefObject<HTMLDivElement | null>;
+  cv?: boolean;
 };
 
-const NavButton: FC<NavButtonProps> = ({ btnName, sectionRef }) => {
+const NavButton: FC<NavButtonProps> = ({ btnName, sectionRef, cv }) => {
   return (
     <Button
       variant="text"
@@ -23,6 +24,12 @@ const NavButton: FC<NavButtonProps> = ({ btnName, sectionRef }) => {
         },
       }}
       onClick={() => {
+        if (cv)
+          window.open(
+            'https://drive.google.com/file/d/1KfT1Oi02fLEnsuZ8D0_VYJMzruvmpdyi/view',
+            '_blank',
+            'noopener,noreferrer',
+          );
         if (sectionRef) {
           if (sectionRef.current) sectionRef.current.scrollIntoView({ behavior: 'smooth' });
         }
